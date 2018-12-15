cd12ca96f0eac3464b7663533d33669de33878b0
dingfeng <1041460265@qq.com>
2018-12-15 20:15:21 +0800

    64%

--- analysis/cnn.py
+++ analysis/cnn.py
@@ -3,7 +3,7 @@
 # author: FD
 import os
 from keras import Sequential
-from keras.layers import LSTM, Masking, Dense,Conv1D,MaxPooling1D,GlobalAveragePooling1D,Dropout,Flatten
+from keras.layers import LSTM, Masking, Dense, Conv1D, MaxPooling1D, GlobalAveragePooling1D, Dropout, Flatten
 from keras.utils import to_categorical
 import numpy as np
 
@@ -19,8 +19,8 @@ def train():
     np.random.shuffle(indexes)
     train_data = train_data[indexes, :, :]
     one_hot_train_labels = to_categorical(train_labels)
-    one_hot_train_labels=one_hot_train_labels[indexes]
-    one_hot_test_labels = to_categorical(test_labels)
+    one_hot_train_labels = one_hot_train_labels[indexes]
+    one_hot_test_labels = to_categorical(test_labels,2)
     model = get_model()
     model.fit(train_data, one_hot_train_labels, batch_size=100, validation_data=(test_data, one_hot_test_labels),
               epochs=1000)
@@ -37,15 +37,14 @@ def get_model():
     model.add(Conv1D(160, 10, activation='relu'))
     model.add(MaxPooling1D(3))
     model.add(Conv1D(32, 7, activation='relu'))
-    model.add(MaxPooling1D(3))
-    model.add(Flatten())
+    model.add(GlobalAveragePooling1D())
     model.add(Dropout(0.2))
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(11, activation='softmax'))
+    model.add(Dense(2, activation='softmax'))
     # print(model.summary())
     model.compile(loss='categorical_crossentropy',
-                    optimizer='adam', metrics=['accuracy'])
+                  optimizer='adam', metrics=['accuracy'])
     return model
 
 
@@ -58,22 +57,26 @@ def get_data():
     count = 0
     label_names = []
     max_len = 0
-    for label_name in os.listdir(dir_path):
+    dirs=os.listdir(dir_path)
+    dirs=['dingfeng','zhangqian']
+    for label_name in dirs:
         label_names.append(label_name)
         label_path = os.path.join(dir_path, label_name)
         filenames = os.listdir(label_path)
         filenames_len = len(filenames)
         indexes = np.arange(filenames_len)
         np.random.shuffle(indexes)
-        train_size = 10
+        train_size = 15
         for i in range(train_size):
             index = indexes[i]
             filename = filenames[index]
             filepath = os.path.join(label_path, filename)
             onedata = np.load(open(filepath, 'rb'))['I']
             for data in onedata:
-                data=data-np.roll(data,1)
-                data=data[1:]
+                data = data - np.roll(data, 1)
+                data = data[1:]
+                data = data - np.roll(data, 1)
+                data = data[1:]
                 data = normalize(data)
                 if data.shape[0] > max_len:
                     max_len = data.shape[0]
@@ -82,22 +85,23 @@ def get_data():
                 # data.resize(3000, 1)
                 train_data.append(data)
                 train_labels.append(count)
-
-        for i in range(train_size, len(indexes)):
-            index = indexes[i]
-            filename = filenames[index]
-            filepath = os.path.join(label_path, filename)
-            onedata = np.load(open(filepath, 'rb'))['I']
-            for data in onedata:
-                data = data - np.roll(data, 1)
-                data = data[1:]
-                data = normalize(data)
-                if data.shape[0] > max_len:
-                    max_len = data.shape[0]
-                data.resize(1700, 1)
-                # data.resize(3000, 1)
-                test_data.append(data)
-                test_labels.append(count)
+            for i in range(train_size, len(indexes)):
+                index = indexes[i]
+                filename = filenames[index]
+                filepath = os.path.join(label_path, filename)
+                onedata = np.load(open(filepath, 'rb'))['I']
+                for data in onedata:
+                    data = data - np.roll(data, 1)
+                    data = data[1:]
+                    data = data - np.roll(data, 1)
+                    data = data[1:]
+                    data = normalize(data)
+                    if data.shape[0] > max_len:
+                        max_len = data.shape[0]
+                    data.resize(1700, 1)
+                    # data.resize(3000, 1)
+                    test_data.append(data)
+                    test_labels.append(count)
         count+=1
     print("labelnames {}".format(label_names))
     train_data = np.asarray(train_data)
